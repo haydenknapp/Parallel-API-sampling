45f7232c429f6eb875bc10685a23dbdfc86e63c6
Hayden Knapp <haydenknapp@gmail.com>
2017-10-25 09:32:54 -0400

    Attempted to do work on master threads

--- test_openmp.c
+++ test_openmp.c
@@ -40,18 +40,36 @@ int main() {
 		printf("Hello world from thread #4\n");
 	}
 
-	/* 2. Add into one integer in a critical. */
+	/* 3. Add into one integer in a critical. */
 	printf("\nAdding into one variable with critical sections...\n");
 	uint8_t shared = 0;
-	#define NUMTHREADS 8
+	#define N_3 8
 	#pragma omp parallel for
-	for (uint8_t threadid = 0; threadid < NUMTHREADS; ++threadid) {
+	for (uint8_t threadid = 0; threadid < N_3; ++threadid) {
 		#pragma omp critical
 		++shared;
 	}
 	printf("Each thread combined incremented \"shared\" to %d.\n", shared);
-	assert(shared == NUMTHREADS);
+	assert(shared == N_3);
 
+	/* 4. Have the master thread stop the other threads. */
+	#define N_4 4
+	#define GOAL_4 256
+	#define MET 1
+	uint8_t met = 0;
+	uint8_t cnt = 0;
 
+	#pragma omp parallel for
+	for (int threadid = 0; threadid < N_4; ++threadid) {
+		#pragma omp critical
+		while (met != MET) {
+			++cnt;
+		}
+		#pragma omp master
+		if (cnt == GOAL_4)
+			met = met;
+	}
+	assert(cnt == GOAL_4);
 }
+
 
